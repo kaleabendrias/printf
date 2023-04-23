454f3dc6a5acb5525b97ecfcea6146cdf7a2ed69
Simi-fo <similoluwa.folorunsho@stu.cu.edu.ng>
2023-04-23 05:53:48 -0700

    conversion specifiers

---
+++ printf_o.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * printf_o - printf function for printing the unsigned int argument
+ * using octal format
+ * @args: input
+ * Return: the number of digits to be printed
+ */
+
+int printf_o(va_list args)
+{
+	unsigned int num, divisor, temp;
+	int digits;
+
+	divisor = 1;
+	num = va_arg(args, unsigned int);
+	temp = num;
+	digits = 0;
+
+	while (temp >= 8)
+	{
+	temp /= 8;
+	divisor *= 8;
+	}
+
+	while (divisor > 0)
+	{
+	int digit = num / divisor;
+
+	_putchar('0' + digit);
+	num -= digit + divisor;
+	divisor /= 8;
+	digits++;
+	}
+
+	return (digits);
+}
