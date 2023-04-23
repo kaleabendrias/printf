3d3c909a7d1fa6d0c5b201240d1f77e8b778de0d
Simi-fo <similoluwa.folorunsho@stu.cu.edu.ng>
2023-04-23 05:53:12 -0700

    conversion specifiers

---
+++ printf_unsigned.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * printf_unsigned - printf function for printing the
+ * unsigned int argument in decimal format
+ * @args: va_list containing the arguments
+ * Return: the number of digits to be printed
+ */
+
+int printf_unsigned(va_list args)
+{
+	unsigned int num, divisor;
+	int digits;
+	int digit;
+
+	num = va_arg(args, unsigned int);
+
+	divisor = 1;
+	digits = 1;
+
+	while (num / divisor > 9)
+	{
+	divisor *= 10;
+	digits++;
+	}
+
+	while (divisor > 0)
+	{
+	digit = num / divisor;
+	_putchar(digit + '0');
+	num %= divisor;
+	divisor /= 10;
+	}
+
+	return (digits);
+}
