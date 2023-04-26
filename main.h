8a4648c50be322714400134a40fda2ac6c36de2d
kaleabendrias <kaleabendrias1212@gmail.com>
2023-04-26 11:16:46 -0700

    fix

--- main.h
+++ main.h
@@ -34,6 +34,7 @@ int printf_hex2(va_list args);
 int printf_S(va_list args);
 int printf_p(va_list args);
 int printf_rev(va_list args);
+int printf_rot(va_list args);
 int _putchar(char c);
 int _printf(const char *format, ...);
 
---
+++ printf_rot.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * printf_rot - prints rot13'ed string
+ * @args: argumrnt
+ * Return: always
+ */
+
+int printf_rot(va_list args)
+{
+	int i, j, c, k;
+	char *s;
+
+	char a[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+	char b[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
+
+	c = 0;
+	k = 0;
+	s = va_arg(args, char *);
+	if (s == NULL)
+		s = "(null)";
+	fo (i = 0; s[i]; i++)
+	{
+		k = 0;
+		for (j = 0; a[j] && !k; j++)
+		{
+			if (s[i] == a[j])
+			{
+				_putchar(beta[j]);
+				c++;
+				k = 1;
+			}
+		}
+		if (!k)
+		{
+			_putchar(s[i]);
+			c++;
+		}
+	}
+	return (c);
+}
+
